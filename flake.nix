90e75180974be73f54c8fd418bd22ea891611725
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-05 22:11:27 +0200

    Add initial flake file

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "MCP Server for YouTube API, enabling video management, Shorts creation, and advanced analytics";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.aarch64-linux.youtube-mcp-server = nixpkgs.legacyPackages.aarch64-linux.buildNpmPackage {
+      pname = "youtube-mcp-server";
+      version = "0.0.1";
+
+      src = ./.;
+
+      npmDepsHash = "sha256-RTROQkoOpRUiBvB1zXittXB1HgtBN+GLTD0tx+ZkWaA=";
+    };
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.mkShell {
+      buildInputs = with nixpkgs.legacyPackages.aarch64-linux; [
+        nodejs
+        prefetch-npm-deps
+      ];
+    };
+
+  };
+}
